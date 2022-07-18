ae16e1bfacccaf6831b7ab6dd1f2ee718d93cdc7
questroo <42195958+questroo@users.noreply.github.com>
2022-07-18 19:50:22 -0400

    Added hand animation for holding the paint palette and putting colors on the brush.
    - Added hand animation for holding the paint palette.
    - Added brush changing colors when touching color on paint palette.

--- art-studio/Assets/Scripts/PaintBrush.cs
+++ art-studio/Assets/Scripts/PaintBrush.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class PaintBrush : MonoBehaviour
 {
-    [SerializeField] private Vector3 positionOffsetFromHand;
-    [SerializeField] private Vector3 rotationOffsetFromHand;
+    [SerializeField] private Material brushMaterial;
+    [SerializeField] private Color baseColor;
+    [SerializeField] private LayerMask colorLayer;
+    [SerializeField] private Collider brushHeadCollider;
+    private Color tempCurrentColor;
+
+    private void Start()
+    {
+        brushMaterial.color = baseColor;
+        tempCurrentColor = baseColor;
+    }
+
+    public void TryAbsorbPaletteColor(Collider palette)
+    {
+        palette.gameObject.TryGetComponent<Renderer>(out Renderer renderer);
+        tempCurrentColor = renderer.material.color;
+        brushMaterial.color = tempCurrentColor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == Mathf.Log(colorLayer.value, 2))
+        {
+            TryAbsorbPaletteColor(other);
+
+        }
+    }
 }
